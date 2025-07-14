c5b1023b80a47573b637bf758a0def2f6a10dd54
nicolerodriguezsmc <rodriguez_nicole_l01@student.smc.edu>
2025-07-13 21:58:27 -0700

    Update assistant.js define constructor PersonalAssistant

--- assistant.js
+++ assistant.js
@@ -4,3 +4,10 @@
 * Module 6b Homework
 * 7/13/2025
 */
+
+//Define constructor PersonalAssistant
+function PersonalAssistant(name) {
+  this.name = name;
+  this.tasks = [];
+  this.mood = "neutral";
+}
